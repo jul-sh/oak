6d8006d2af4a0b13483c7193ffaab04ccbb3c2fe
Juliette Pretot <julsh@google.com>
2021-12-21 14:41:03 +0000

    use pregenerated rust proto code

--- third_party/vendor/tract-tensorflow-0.15.4/build.rs
+++ third_party/vendor/tract-tensorflow-0.15.4/build.rs
@@ -3,7 +3,10 @@ use std::{env, fs, path};
 fn main() -> std::io::Result<()> {
     let mut inputs: Vec<path::PathBuf> = vec![];
 
-    for dir in &["protos/tensorflow/core/framework", "protos/tensorflow/core/protobuf"] {
+    for dir in &[
+        "protos/tensorflow/core/framework",
+        "protos/tensorflow/core/protobuf",
+    ] {
         for pb in fs::read_dir(dir)? {
             inputs.push(pb?.path())
         }
@@ -11,9 +14,20 @@ fn main() -> std::io::Result<()> {
 
     let gen = path::PathBuf::from(env::var("OUT_DIR").unwrap()).join("prost");
     let _ = fs::create_dir_all(&gen);
-    prost_build::Config::new()
-        .out_dir(gen)
-        .compile_protos(&inputs, &[path::PathBuf::from("protos/")])?;
+
+    fs::copy(
+        "protos/tensorflow/core/pregenerated/tensorflow.rs",
+        path::PathBuf::from(env::var("OUT_DIR").unwrap())
+            .join("prost")
+            .join("tensorflow.rs"),
+    )?;
+
+    fs::copy(
+        "protos/tensorflow/core/pregenerated/google.protobuf.rs",
+        path::PathBuf::from(env::var("OUT_DIR").unwrap())
+            .join("prost")
+            .join("google.protobuf.rs"),
+    )?;
 
     Ok(())
 }
