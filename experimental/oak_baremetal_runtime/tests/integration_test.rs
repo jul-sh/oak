0d92bf3890f73a6d6847411102274156b103dc42
Juliette Pretot <julsh@google.com>
2022-08-08 17:04:13 +0000

    test that user requests are handled after initialization

--- experimental/oak_baremetal_runtime/tests/integration_test.rs
+++ experimental/oak_baremetal_runtime/tests/integration_test.rs
@@ -14,7 +14,7 @@
 // limitations under the License.
 //
 
-use oak_baremetal_runtime::schema::TrustedRuntime;
+use oak_baremetal_runtime::{schema::TrustedRuntime, RuntimeImplementation};
 use oak_remote_attestation::handshaker::{
     AttestationBehavior, ClientHandshaker, EmptyAttestationVerifier,
 };
@@ -29,12 +29,14 @@ mod schema {
 }
 
 const MOCK_SESSION_ID: &[u8; 8] = &[0, 0, 0, 0, 0, 0, 0, 0];
+const MOCK_CONSTANT_RESPONSE_SIZE: u32 = 1024;
+const ECHO_WASM_BYTES: &[u8] = include_bytes!("echo.wasm");
 
 #[test]
 fn it_should_not_handle_user_requests_before_initialization() {
     let attestation_behavior =
         AttestationBehavior::create(PlaceholderAmdAttestationGenerator, EmptyAttestationVerifier);
-    let runtime = oak_baremetal_runtime::RuntimeImplementation::new(attestation_behavior.clone());
+    let runtime = RuntimeImplementation::new(attestation_behavior.clone());
     let mut client = schema::TrustedRuntimeClient::new(TrustedRuntime::serve(runtime));
 
     let mut handshaker =
@@ -67,4 +69,60 @@ fn it_should_not_handle_user_requests_before_initialization() {
         result.unwrap_err(),
         oak_idl::Status::new(oak_idl::StatusCode::FailedPrecondition)
     );
+}
+
+#[test]
+fn it_should_handle_user_requests_after_initialization() {
+    let attestation_behavior =
+        AttestationBehavior::create(PlaceholderAmdAttestationGenerator, EmptyAttestationVerifier);
+    let runtime = oak_baremetal_runtime::RuntimeImplementation::new(attestation_behavior.clone());
+    let mut client = schema::TrustedRuntimeClient::new(TrustedRuntime::serve(runtime));
+
+    let owned_initialization_flatbuffer = {
+        let mut builder = oak_idl::utils::OwnedFlatbufferBuilder::default();
+        let wasm_module = builder.create_vector::<u8>(ECHO_WASM_BYTES);
+        let initialization_flatbuffer = schema::Initialization::create(
+            &mut builder,
+            &schema::InitializationArgs {
+                wasm_module: Some(wasm_module),
+                constant_response_size: MOCK_CONSTANT_RESPONSE_SIZE,
+            },
+        );
+
+        builder
+            .finish(initialization_flatbuffer)
+            .expect("errored when creating initialization message")
+    };
+
+    client
+        .initialize(owned_initialization_flatbuffer.into_vec())
+        .unwrap();
+
+    let mut handshaker =
+        ClientHandshaker::new(attestation_behavior).expect("could not create client handshaker");
+    let client_hello = handshaker
+        .create_client_hello()
+        .expect("Couldn't create client hello message");
+
+    let owned_request_flatbuffer = {
+        let mut builder = oak_idl::utils::OwnedFlatbufferBuilder::default();
+        let session_id = &schema::SessionId::new(MOCK_SESSION_ID);
+        let body = builder.create_vector::<u8>(&client_hello);
+        let flatbuffer = schema::UserRequest::create(
+            &mut builder,
+            &schema::UserRequestArgs {
+                session_id: Some(session_id),
+                body: Some(body),
+            },
+        );
+        builder
+            .finish(flatbuffer)
+            .map_err(|err| {
+                oak_idl::Status::new_with_message(oak_idl::StatusCode::Internal, err.to_string())
+            })
+            .unwrap()
+    };
+    let result = client.handle_user_request(owned_request_flatbuffer.into_vec());
+
+    result.unwrap();
 }
