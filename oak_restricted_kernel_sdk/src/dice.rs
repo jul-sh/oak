49f15eb370f5a30648d4de9b51abf4829b5e4601
Juliette Pretot <julsh@google.com>
2023-12-08 22:02:35 +0000

    Stop exposing dice related structs in the SDK
    The SDK should be agnostic about which underlying attesation method is used. Hence dice related structs are removed. oak functions still depends on them, so the relevant code has been moved to continue supporting it. In the future it should be refactored to use the SDK.

--- oak_restricted_kernel_sdk/src/dice.rs
+++ oak_restricted_kernel_sdk/src/dice.rs
@@ -15,76 +15,12 @@
 //
 
 use crate::syscall::read;
-use oak_crypto::{
-    encryptor::EncryptionKeyProvider,
-    hpke::{Deserializable, PrivateKey, PublicKey},
-};
-use oak_dice::{
-    cert::{
-        cose_key_to_hpke_public_key, get_claims_set_from_certificate_bytes,
-        get_public_key_from_claims_set,
-    },
-    evidence::{RestrictedKernelDiceData, P256_PRIVATE_KEY_SIZE, X25519_PRIVATE_KEY_SIZE},
-};
-use oak_remote_attestation::proto::oak::attestation::v1::Evidence;
+use oak_dice::evidence::{RestrictedKernelDiceData, P256_PRIVATE_KEY_SIZE};
 use oak_restricted_kernel_interface::DICE_DATA_FD;
 use p256::ecdsa::SigningKey;
 use zerocopy::{AsBytes, FromZeroes};
 
-/// Wrapper for DICE evidence and application private keys.
-pub struct DiceWrapper {
-    pub evidence: Evidence,
-    pub encryption_key: EncryptionKeyProvider,
-    pub signer: Signer,
-}
-
-impl DiceWrapper {
-    pub fn try_create() -> anyhow::Result<Self> {
-        get_dice_evidence_and_keys()
-    }
-}
-
-/// Get the DICE evidence and application private keys from the Restricted Kernel
-fn get_dice_evidence_and_keys() -> anyhow::Result<DiceWrapper> {
-    let dice_data = get_restricted_kernel_dice_data()?;
-    let evidence: Evidence = dice_data.evidence.try_into()?;
-    let private_key = PrivateKey::from_bytes(
-        &dice_data.application_private_keys.encryption_private_key[..X25519_PRIVATE_KEY_SIZE],
-    )
-    .map_err(|error| anyhow::anyhow!("couldn't deserialize private key: {}", error))?;
-    let application_keys = evidence
-        .application_keys
-        .as_ref()
-        .ok_or_else(|| anyhow::anyhow!("no application keys"))?;
-    let claims =
-        get_claims_set_from_certificate_bytes(application_keys.encryption_public_key_certificate())
-            .map_err(|err| {
-                anyhow::anyhow!("couldn't parse encryption public key certificate: {err}")
-            })?;
-    let public_key = get_public_key_from_claims_set(&claims)
-        .map_err(|err| anyhow::anyhow!("couldn't get public key from certificate: {err}"))?;
-    let public_key = cose_key_to_hpke_public_key(&public_key)
-        .map_err(|err| anyhow::anyhow!("couldn't extract public key: {err}"))?;
-    let encryption_key = EncryptionKeyProvider::new(
-        private_key,
-        PublicKey::from_bytes(&public_key)
-            .map_err(|err| anyhow::anyhow!("couldn't decode public key: {err}"))?,
-    );
-    let signer = {
-        let key = SigningKey::from_slice(
-            &dice_data.application_private_keys.signing_private_key[..P256_PRIVATE_KEY_SIZE],
-        )
-        .map_err(|error| anyhow::anyhow!("couldn't deserialize signing key: {}", error))?;
-        Signer { key }
-    };
-
-    Ok(DiceWrapper {
-        evidence,
-        encryption_key,
-        signer,
-    })
-}
-
+#[allow(dead_code)]
 fn get_restricted_kernel_dice_data() -> anyhow::Result<RestrictedKernelDiceData> {
     let mut result = RestrictedKernelDiceData::new_zeroed();
     let buffer = result.as_bytes_mut();
@@ -102,5 +38,18 @@ pub struct Signer {
 impl Signer {
     pub fn sign(&self, message: &[u8]) -> oak_crypto::signer::Signature {
         <SigningKey as oak_crypto::signer::Signer>::sign(&self.key, message)
+    }
+}
+
+impl TryFrom<&oak_dice::evidence::RestrictedKernelDiceData> for Signer {
+    type Error = anyhow::Error;
+    fn try_from(
+        dice_data: &oak_dice::evidence::RestrictedKernelDiceData,
+    ) -> Result<Self, Self::Error> {
+        let key = SigningKey::from_slice(
+            &dice_data.application_private_keys.signing_private_key[..P256_PRIVATE_KEY_SIZE],
+        )
+        .map_err(|error| anyhow::anyhow!("couldn't deserialize signing key: {}", error))?;
+        Ok(Signer { key })
     }
 }
